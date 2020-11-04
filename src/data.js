38788b0411737627a6e82c62e45c26e680ed5ad6
Karolin Frennert <karolinfrennert@gmail.com>
2020-11-04 17:41:31 +0100

    First commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+//My projects 
+
+export const projects = 
+[
+  {
+    "name": "Kurts Portfolio",
+    "technology": [
+      "CSS",
+      "JS",
+      "HTML"
+    ],
+    "video": "https://link-to-video.com/kurt.mp4",
+    "description": "Kurts Portfolio with beautiful colors",
+    "github": "https://github.com/Kurts-portfolio",
+    "picture": "Kurt.img",
+    "rating": 10
+  },
+  {
+    "name": "Habit Tracker",
+    "technology": [
+      "CSS",
+      "JS",
+      "HTML",
+      "Firebase"
+    ],
+    "video": "https://link-to-video.com/habitTracker.mp4",
+    "description": "Habit tracker to track your weekly habits",
+    "github": "https://github.com/habit-tracker",
+    "picture": "tracker.img",
+    "rating": 7
+  },
+  {
+    "name": "Game of 21",
+    "technology": [
+      "Java"
+    ],
+    "video": "https://link-to-video.com/game21.mp4",
+    "description": "Game of 21 to play against the machine",
+    "github": "https://github.com/game-of-21",
+    "picture": "game.img",
+    "rating": 9
+  }
+]
