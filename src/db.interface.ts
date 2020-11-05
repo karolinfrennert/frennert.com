fa2291d3682abd7abcd81fe4e78d318da504150c
Karolin Frennert <karolinfrennert@gmail.com>
2020-11-05 22:09:30 +0100

    Footer + Nav in ts

--- src/db.interface.ts
+++ src/db.interface.ts
@@ -1,17 +1,25 @@
-export interface Data {
+import { type } from "os";
 
+export interface Data {
   firstName: string;
   lastName: string;
   email: string;
   fullName: string;
-  project: [{
-    name: string;
-    technology: string[];
-    description: string;
-    github: string;
+  mainMenu: MenuItems[];
+  footer: MenuItems[];
+};
 
-  }];
-
-
-
+export interface Project {
+  name: string;
+  technology: string[];
+  description: string;
+  github: string;
 }
+
+export type Projects = Project[];
+
+type MenuItems = {
+  href: string;
+  text: string;
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,103 @@
+import { Project } from './db.interface'
 import { Data } from './db.interface'
+import { Projects } from './db.interface'
+
+const KurtPortfolio: Projects = [
+  {
+    name: 'Kurt',
+    technology: ['CSS', 'JS', 'TypeScript'],
+    description: 'Every cat deserves a portfolio',
+    github: 'https://github.com/karolinfrennert/portfolio-Kurt',
+  }
+]
 
 const aboutMe: Data = {
   firstName: 'Karolin',
   lastName: 'Frennert',
   email: 'karolin@frennert.com',
   fullName: '${firstName} ${lastName}',
-  project: [{
-    name: 'Kurt',
-    technology: ['CSS', 'JS', 'TypeScript'],
-    description: 'Every cat deserves a portfolio',
-    github: 'https://github.com/karolinfrennert/portfolio-Kurt',
-  }]
+  mainMenu: [
+    {
+      href: './index.html',
+      text: 'Home'
+    },
+    {
+      href: '#about',
+      text: 'About'
+    },
+    {
+      href: '#projects',
+      text: 'Projects'
+    },
+    {
+      href: './contact.html',
+      text: 'Contact'
+    },
+  ],
+
+  footer: [
+    {
+      href: 'https://github.com/karolinfrennert',
+      text: 'GitHub'
+    },
+    {
+      href: 'mailto: karolin@frennert.com',
+      text: 'Email',
+    },
+    {
+      href: 'https://www.linkedin.com/in/karolin-frennert-59a09b132/',
+      text: 'LinkedIn',
+
+    }
+  ]
 }
 
-console.log("hay");
-console.log(aboutMe.firstName);
+
+const supportsTemplate = function () {
+  return 'content' in document.createElement('template');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (supportsTemplate()) {
+    console.log('hey');
+    const navbar = document.getElementById('navbar');
+    console.dir(navbar);
+    let content = navbar.content;
+    console.log(content);
+    document.body.appendChild(content);
+  }
+})
+
+const addLink = (href: string, text: string) => {
+  const link = document.createElement('a');
+  link.innerText = text;
+  link.href = href;
+  return link;
+}
+
+//Ceating Nav
+const navBar = document.createElement('nav');
+navBar.classList.add('main-nav');
+const addLinkToNavBar = (element: HTMLAnchorElement) => {
+  navBar.appendChild(element);
+}
+
+aboutMe.mainMenu.forEach(menuItem => {
+  addLinkToNavBar(addLink(menuItem.href, menuItem.text));
+});
+
+document.body.insertAdjacentElement('afterbegin', navBar)
+
+//Create Footer
+const footer = document.createElement('footer');
+footer.classList.add('main-footer');
+
+const addLinkToFooter = (element: HTMLAnchorElement) => {
+  footer.appendChild(element);
+}
+
+aboutMe.footer.forEach(footerItem => {
+  addLinkToFooter(addLink(footerItem.href, footerItem.text))
+});
+
+document.body.insertAdjacentElement('beforeend', footer);
